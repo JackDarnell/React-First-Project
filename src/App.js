932d0786a8770b21d62fe81690033cec9afde6b7
jack <darnelljack@icloud.com>
2022-12-21 18:34:37 -0500

    learned some mor jsx aspects

--- src/App.js
+++ src/App.js
@@ -9,6 +9,14 @@ function App() {
     { id: "e4", title: "New Desk (Wooden)", amount: 450, date: new Date(2021, 5, 12) },
   ];
 
+  // return React.createElement(
+  //   'div', 
+  //   {}, 
+  //   React.createElement('h2', {}, "Lets get started"), 
+  //   React.createElement(Expenses, {expenses: expenses})
+  // ); //3 arguments.
+
+  // JSX is converted into react code, and then that is converted into browser code.
   return (
     <div>
       <h2>Let's get started!</h2>
