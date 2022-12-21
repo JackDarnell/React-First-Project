f957f7923a624578d2ca7681093751e46a3ffc50
jack <darnelljack@icloud.com>
2022-12-21 18:38:32 -0500

    better organization

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
-import Card from './Card';
+import Card from '../UI/Card';
 
 //a component is just a function
 //we can use the function to return some jsx code
--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
-import Card from './Card';
+import Card from '../UI/Card';
 
 //a component is just a function
 //we can use the function to return some jsx code
