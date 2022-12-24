f26dcb03032f557020b509e8f2fce67cce148d98
jack <darnelljack@icloud.com>
2022-12-23 20:00:47 -0500

    added filter that transfers data up to expenses, still need to implement UI changes for when the user changes the filter

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -2,14 +2,19 @@ import React from 'react';
 import './Expenses.css';
 import ExpenseItem from './ExpenseItem';
 import Card from '../UI/Card';
+import ExpensesFilter from '../ExpenseFilter/ExpensesFilter';
 
 //a component is just a function
 //we can use the function to return some jsx code
+function expenseFilterDataHandler(expenseFilterData) {
+    console.log(expenseFilterData);
+}
 
 //props stands for properties
 function Expenses(props) {
     return (
         <Card className='expenses'>
+            <ExpensesFilter onYearChange={expenseFilterDataHandler}></ExpensesFilter>
             <ExpenseItem title = {props.expenses[0].title} amount = {props.expenses[0].amount} date = {props.expenses[0].date}></ExpenseItem>
             <ExpenseItem title = {props.expenses[1].title} amount = {props.expenses[1].amount} date = {props.expenses[1].date}></ExpenseItem>
             <ExpenseItem title = {props.expenses[2].title} amount = {props.expenses[2].amount} date = {props.expenses[2].date}></ExpenseItem>
